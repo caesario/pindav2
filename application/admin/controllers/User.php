a73532984ffe8c005b2b7275d7d63368d360ccd2
Ricky Ariansyah <rickyarians@outlook.com>
2019-04-13 14:14:38 +0700

    update user Delete And Read List

---
+++ application/admin/controllers/User.php
@@ -0,0 +1,222 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+//		$this->not_logged_in();
+
+	}
+
+    public function index()
+    {
+        $this->data['page_title'] = 'User';
+        $this->render_template('user/index', $this->data);
+    }
+
+
+    public function edit($id)
+    {
+        $this->data['page_title'] = 'Edit Artikel';
+        $this->data['detailartikel'] = $this->artikel->getArtikelDataDetail($id);
+        $this->data['admin'] = $this->admin->getAdminData();
+        $this->render_template('artikel/edit', $this->data);
+    }
+
+
+    public function update() {
+
+        $this->form_validation->set_rules('id_artikel', 'ID Artikel', 'trim');
+        $this->form_validation->set_rules('judul_artikel', 'Judul Artikel', 'trim');
+        $this->form_validation->set_rules('isi_artikel', 'Isi Artikel', 'trim');
+        $this->form_validation->set_rules('tanggal', 'Tanggal Waktu', 'trim');
+        $this->form_validation->set_rules('id_admin', 'Admin', 'trim');
+        $this->form_validation->set_error_delimiters('<p class="text-danger">','</p>');
+
+
+        if ($this->input->post('tanggal')!= null){
+            $myTime = strtotime($this->input->post('tanggal'));
+            $tanggal= date("Y-m-d H:i:s", $myTime);
+        } else {
+
+            $tanggal = $this->input->post('tanggal_seb');
+        }
+        $id_artikel = $this->input->post('id_artikel');
+        if ($this->form_validation->run() == TRUE) {
+            // true case
+
+
+
+
+            $data = array(
+                'judul_artikel' => $this->input->post('judul_artikel'),
+                'isi_artikel' => $this->input->post('isi_artikel'),
+                'tgl_artikel' => $tanggal,
+                'id_admin' => $this->input->post('id_admin'),
+                'UpdateTime' => date("Y-m-d h:i:s")
+            );
+
+
+            if($_FILES['artikel_gambar']['size'] > 0) {
+                $upload_image = $this->upload_gambarartikel();
+                $upload_image = array('gambar_artikel' => $upload_image);
+
+                $this->artikel->update($upload_image, $id_artikel);
+            }
+
+            $update = $this->artikel->update($data, $id_artikel);
+            if($update == true) {
+                $this->session->set_flashdata('success', 'Successfully updated');
+                redirect('Article', 'refresh');
+            }
+            else {
+                $this->session->set_flashdata('errors', 'Error occurred!!');
+                redirect('Article/edit'.$id_artikel, 'refresh');
+            }
+        }
+        else {
+            $this->session->set_flashdata('errors', 'Error occurred!!');
+            redirect('Article/edit'.$id_artikel, 'refresh');
+        }
+    }
+	/* 
+	* It only redirects to the manage category page
+	*/
+
+
+
+    public function fetchUserData()
+    {
+        $result = array('data' => array());
+
+        $data = $this->user->getUserData();
+
+        foreach ($data as $key => $value) {
+            // button
+            $buttons = '';
+
+
+            $buttons .= '<button type="button" class="btn btn-warning" onclick="editCategory(&quot;'.$value['id_user'].'&quot;)" data-toggle="modal" data-target="#editModal"><i class="fas fa-pencil-alt"></i></button>';
+
+            $buttons .= ' <button type="button" class="btn btn-danger" onclick="removeFunc(&quot;'.$value['id_user'].'&quot;)" data-toggle="modal" data-target="#removeUserModal"><i class="fa fa-trash"></i></button>';
+
+
+
+
+            $result['data'][$key] = array(
+                $value['id_user'],
+                $value['nama_user'],
+                $value['username'],
+                $value['email'],
+                $buttons
+            );
+        } // /foreach
+
+        echo json_encode($result);
+    }
+
+    public function add() {
+
+        $response = array();
+        $id = uniqid('USR-');
+        $this->form_validation->set_rules('judul_artikel', 'Judul Artikel', 'trim|required');
+        $this->form_validation->set_rules('isi_artikel', 'Isi Artikel', 'trim|required');
+        $this->form_validation->set_rules('tanggal', 'Tanggal Waktu', 'trim|required');
+        $this->form_validation->set_rules('id_admin', 'Admin', 'trim|required');
+        $this->form_validation->set_error_delimiters('<p class="text-danger">','</p>');
+
+        $myTime = strtotime($this->input->post('tanggal'));
+        $time = date("Y-m-d H:i:s", $myTime);
+        if ($this->form_validation->run() == TRUE) {
+
+            $uploadgambar = $this->upload_gambarartikel();
+            $data = array(
+                'id_artikel' => $id,
+                'judul_artikel' => $this->input->post('judul_artikel'),
+                'isi_artikel' => $this->input->post('isi_artikel'),
+                'tgl_artikel' => $time,
+                'id_admin' => $this->input->post('id_admin'),
+                'gambar_artikel' => $uploadgambar,
+                'CreateTime' =>  date('Y-m-d H:i:s')
+            );
+
+            $create = $this->artikel->create($data);
+            if($create == true) {
+                $this->session->set_flashdata('success', 'Artikel Berhasil Ditambah');
+                redirect('Article', 'refresh');
+            }
+            else {
+                $this->session->set_flashdata('error', 'Artikel gagal Ditambah');
+                redirect('Article', 'refresh');
+            }
+        }
+        else {
+            $this->data['page_title'] = 'Tambah Artikel';
+            $this->data['admin'] = $this->admin->getAdminData();
+            $this->render_template('artikel/add.php', $this->data);
+        }
+
+    }
+
+
+
+    public function remove()
+    {
+
+        $user_id = $this->input->post('user_id');
+        $response = array();
+        if( $user_id ) {
+            $delete = $this->user->remove($user_id );
+
+            if($delete == true) {
+                $response['success'] = true;
+                $response['messages'] = "Successfully removed";
+            }
+            else {
+                $response['success'] = false;
+                $response['messages'] = "Error in the database while removing the brand information";
+            }
+        }
+        else {
+            $response['success'] = false;
+            $response['messages'] = "Refersh the page again!!";
+        }
+
+        echo json_encode($response);
+    }
+
+
+
+    public function upload_gambarartikel()
+    {
+        // assets/images/product_image
+        $config['upload_path'] = 'assets/admin/gambarartikel';
+        $config['file_name'] =  uniqid('art');
+        $config['allowed_types'] = 'gif|jpg|png|svg';
+        $config['max_size'] = '1000';
+
+        // $config['max_width']  = '1024';s
+        // $config['max_height']  = '768';
+
+        $this->load->library('upload', $config);
+        if ( ! $this->upload->do_upload('artikel_gambar'))
+        {
+            $error = $this->upload->display_errors();
+            return $error;
+        }
+        else
+        {
+            $data = array('upload_data' => $this->upload->data());
+            $type = explode('.', $_FILES['artikel_gambar']['name']);
+            $type = $type[count($type) - 1];
+
+            $path = $config['upload_path'].'/'.$config['file_name'].'.'.$type;
+            return ($data == true) ? $path : false;
+        }
+    }
+
+}
---
+++ application/admin/views/user/index.php
@@ -0,0 +1,474 @@
+<!-- page content -->
+    <div class="right_col" role="main">
+
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>User <small>Daftar</small></h3>
+                </div>
+            </div>
+
+            <div class="clearfix"></div>
+            <br>
+            <button class="btn btn-success" data-toggle="modal" data-target="#addModal">Tambah User</button>
+            <br>
+            <br>
+
+            <!-- Notification -->
+            <div id="messages"></div>
+            <?php if($this->session->flashdata('success')): ?>
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <?php echo $this->session->flashdata('success'); ?>
+                </div>
+            <?php elseif($this->session->flashdata('error')): ?>
+                <div class="alert alert-error alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <?php echo $this->session->flashdata('error'); ?>
+                </div>
+            <?php endif; ?>
+            <!-- End Notification -->
+
+            <!--   Tabel konten         -->
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>User <small>List</small></h2>
+                                <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
+                            <table id="UserTable" class="table table-striped table-bordered table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th>ID User</th>
+                                        <th>Nama user</th>
+                                        <th>username</th>
+                                        <th>email</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--   Tabel konten         -->
+        </div>
+    </div>
+<!-- /page content -->
+
+
+<!-- Modal Section -->
+<!-- create kategori modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="addModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Tambah Kategori</h4>
+            </div>
+            <form role="form" action="<?php echo site_url('Category/tambah') ?>" method="post" id="createForm" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nama_kategori">Nama Kategori</label>
+                        <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" placeholder="Masukkan Kategori Proyek" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="status">Status</label>
+                        <select class="form-control" id="status" name="status">
+                            <option value="1">Aktif</option>
+                            <option value="2">Tidak Aktif</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="icon">Icon</label>
+                        <select class="form-control" id="icon" name="icon">
+                            <?php foreach ($icon as $k => $v): ?>
+
+                                <option value="<?php echo $v['id_icon'] ?>"><?php echo $v['nama_icon'] ?></option>
+
+                            <?php endforeach ?>
+                        </select>
+                    </div>
+                    <br>
+                    <br>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Simpan</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<!-- create icon -->
+<div class="modal fade" tabindex="-1" role="dialog" id="addIcon">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Tambah icon</h4>
+            </div>
+            <form role="form" action="<?php echo site_url('Category/tambahicon') ?>" method="post" id="createForm" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="product_image">Gambar / Foto</label>
+                        <div class="kv-avatar">
+                            <div class="file-loading">
+                                <input id="iconcat" name="iconcat" type="file">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_icon">Nama Icon</label>
+                        <input type="text" class="form-control" id="nama_icon" name="nama_icon" placeholder="Masukkan Nama Icon" autocomplete="off">
+                    </div>
+                    <br>
+                    <br>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Simpan</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!---->
+<!-- edit brand modal -->
+
+<div class="modal fade" tabindex="-1" role="dialog" id="editModal">
+
+    <div class="modal-dialog" role="document">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                <h4 class="modal-title">Ubah Merk</h4>
+
+            </div>
+
+
+
+            <form role="form" action="<?php echo site_url('Category/update') ?>" method="post" id="updateBrandForm">
+
+
+
+                <div class="modal-body">
+
+                    <div id="messages"></div>
+
+
+
+                    <div class="form-group">
+
+                        <label for="edit_brand_name">Nama Merk</label>
+
+                        <input type="text" class="form-control" id="edit_category_name" name="edit_category_name" placeholder="Masukkan Nama Merk" autocomplete="off">
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <label for="edit_active">Status</label>
+
+                        <select class="form-control" id="edit_status" name="edit_status">
+
+                            <option value="1">Aktif</option>
+
+                            <option value="2">Tidak Aktif</option>
+
+                        </select>
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="icon">Icon</label>
+                        <select class="form-control" id="edit_icon" name="edit_icon">
+                            <?php foreach ($icon as $k => $v): ?>
+
+                                <option value="<?php echo $v['id_icon'] ?>"><?php echo $v['nama_icon'] ?></option>
+
+                            <?php endforeach ?>
+                        </select>
+                    </div>
+
+                </div>
+
+
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+
+                </div>
+
+
+
+            </form>
+
+
+
+
+
+        </div><!-- /.modal-content -->
+
+    </div><!-- /.modal-dialog -->
+
+</div><!-- /.modal -->
+
+
+<!-- remove brand modal -->
+
+<div class="modal fade" tabindex="-1" role="dialog" id="removeUserModal">
+
+    <div class="modal-dialog" role="document">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                <h4 class="modal-title">Hapus User</h4>
+
+            </div>
+
+
+
+            <form role="form" action="<?php echo site_url('User/remove') ?>" method="post" id="removeUserForm">
+
+                <div class="modal-body">
+
+                    <p>Apakah Anda yakin untuk menghapus user ini?</p>
+
+                </div>
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                    <button type="submit" class="btn btn-primary">Hapus</button>
+
+                </div>
+
+            </form>
+
+
+
+
+
+        </div><!-- /.modal-content -->
+
+    </div><!-- /.modal-dialog -->
+
+</div><!-- /.modal -->
+
+<!-- End Modal Section -->
+
+
+<!-- footer content -->
+    <footer>
+        <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+        </div>
+        <div class="clearfix"></div>
+    </footer>
+<!-- /footer content -->
+
+
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="<?= base_url('assets/admin/vendors/jquery/dist/jquery.min.js') ?>"></script>
+<!-- Bootstrap -->
+<script src="<?= base_url('assets/admin/vendors/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<!-- FastClick -->
+<script src="<?= base_url('assets/admin/vendors/fastclick/lib/fastclick.js')?>"></script>
+<!-- NProgress -->
+<script src="<?= base_url('assets/admin/vendors/nprogress/nprogress.js') ?>"></script>
+<!-- Chart.js -->
+<script src="<?= base_url('assets/admin/vendors/Chart.js/dist/Chart.min.js') ?>"></script>
+<!-- gauge.js -->
+<script src="<?= base_url('assets/admin/vendors/gauge.js/dist/gauge.min.js') ?>"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?= base_url('assets/admin/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')?>"></script>
+<!-- iCheck -->
+<script src="<?= base_url('assets/admin/vendors/iCheck/icheck.min.js') ?>"></script>
+<!-- Skycons -->
+<script src="<?= base_url('assets/admin/vendors/skycons/skycons.js') ?>"></script>
+<!-- Flot -->
+<script src="<?= base_url('assets/admin/vendors/Flot/jquery.flot.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/Flot/jquery.flot.pie.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/Flot/jquery.flot.time.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/Flot/jquery.flot.stack.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/Flot/jquery.flot.resize.js') ?>"></script>
+<!-- Flot plugins -->
+<script src="<?= base_url('assets/admin/vendors/flot.orderbars/js/jquery.flot.orderBars.js')?>"></script>
+<script src="<?= base_url('assets/admin/vendors/flot-spline/js/jquery.flot.spline.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/flot.curvedlines/curvedLines.js')?>"></script>
+<!-- DateJS -->
+<script src="<?= base_url('assets/admin/vendors/DateJS/build/date.js')?>"></script>
+<!-- JQVMap -->
+<script src="<?= base_url('assets/admin/vendors/jqvmap/dist/jquery.vmap.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/jqvmap/dist/maps/jquery.vmap.world.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') ?>"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="<?= base_url('assets/admin/vendors/moment/min/moment.min.js') ?>"></script>
+<script src="<?= base_url('assets/admin/vendors/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="<?= base_url('assets/admin/build/js/custom.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/admin/vendors/fileinput/fileinput.min.js') ?>"></script>
+
+<!-- Datatables -->
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-buttons/js/dataTables.buttons.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-buttons/js/buttons.flash.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-buttons/js/buttons.html5.min.js')?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-buttons/js/buttons.print.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-responsive/js/dataTables.responsive.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/datatables.net-scroller/js/dataTables.scroller.min.js')?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/jszip/dist/jszip.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/pdfmake/build/pdfmake.min.js') ?>"></script>
+
+<script src="<?= base_url('assets/admin/vendors/pdfmake/build/vfs_fonts.js') ?>"></script>
+
+    <script type="text/javascript">
+
+        var manageTable;
+
+        $(document).ready(function() {
+            manageTable = $('#UserTable').DataTable({
+
+                'ajax': 'User/fetchUserData',
+
+                'order': []
+
+            });
+
+        });
+
+        // remove functions
+        function removeFunc(id)
+
+        {
+
+            if(id) {
+
+                $("#removeUserForm").on('submit', function() {
+
+
+
+                    var form = $(this);
+
+
+
+                    // remove the text-danger
+
+                    $(".text-danger").remove();
+
+
+
+                    $.ajax({
+
+                        url: form.attr('action'),
+
+                        type: form.attr('method'),
+
+                        data: { user_id:id },
+
+                        dataType: 'json',
+
+                        success:function(response) {
+
+
+
+                            manageTable.ajax.reload(null, false);
+
+
+
+                            if(response.success === true) {
+
+                                $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+
+                                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+
+                                    '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+
+                                    '</div>');
+
+
+
+                                // hide the modal
+
+                                $("#removeUserModal").modal('hide');
+
+                                $('.modal-backdrop').remove();
+
+
+
+                            } else {
+
+
+
+                                $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+
+                                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+
+                                    '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+
+                                    '</div>');
+                                $("#removeUserModal").modal('hide');
+
+                                $('.modal-backdrop').remove();
+
+                            }
+
+                        }
+
+                    });
+
+
+
+                    return false;
+
+                });
+
+            }
+
+        }
+
+</script>
+
+
+</body>
+</html>
